ce72a583c2e6c90847815578bfc6e9a3dffab7fd
binking <jiangzhibin2014.xujie@gmail.com>
2017-01-09 15:49:19 +0800

    debug

--- weibo_mid_spider.py
+++ weibo_mid_spider.py
@@ -23,6 +23,7 @@ class WeiboMidSpider(WeiboSpider):
         if len(self.page) < 20000:
             return mid
         mid_rexp = re.findall(r'mid=(\d{16})', self.page)
+        import ipdb; ipdb.set_trace()
         if mid_rexp:
             mid_group = Counter([mid.group(1) for mid in mid_rexp])
             print mid_group
